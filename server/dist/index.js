7b1fafd642818804cb53d8d23e6c009e9b8ae9e9
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2018-07-02 16:48:59 +0200

    Ignore dist folder and use express

--- server/dist/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var foo = function foo() {
-  return console.log('The best function ever');
-};
-foo();
--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,8 @@
-const foo = () => console.log('The best function ever');
-foo();
+import express from 'express';
+
+const app = express();
+app.listen(8000, () => {
+    console.log('Server started, listening on port 8000');
+});
+
+app.route('/api/hello').get((req, res) => res.send('Hello world'));
