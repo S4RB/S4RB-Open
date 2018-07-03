1c32bfae18c8c169645dada60d06cc77d68ac5c5
Jan Gutowski <guko44+altimi@gmail.com>
2018-07-03 09:02:10 +0200

    additional test of parser

--- app/backend/csv2json/parser.js
+++ app/backend/csv2json/parser.js
@@ -3,8 +3,11 @@ const _ = require('lodash');
 module.exports = parse;
 
 function parse(text) {
+    if (!_.isString(text)) return;
     const lines = _.chain(text).replace(/"/g, '').split(/\n/).value();
     const headers = _.split(lines.shift(), ',');
+
+    if (_.isEmpty(headers) || _.isEmpty(lines[0])) return [];
 
     return _.chain(lines)
             .split(',')
--- app/backend/csv2json/parser.test.js
+++ app/backend/csv2json/parser.test.js
@@ -7,21 +7,74 @@ describe('#parser', () => {
 
     beforeEach(() => {
         suite = {};
-        suite.testData = `"h1","h2","h3"\n"data1","data2","data3"\n"data4","data5","data6"`;
     });
 
     afterEach(() => {
         suite = null;
     });
 
-    it('should add', () => {
-        const expectedResult = [
-            {"h1":"data1","h2":"data2","h3":"data3"},
-            {"h1":"data4","h2":"data5","h3":"data6"}
-        ];
+    describe('should return proper value when', () => {
 
-        const actualResult = parse(suite.testData);
-        
-        assert.deepEqual(expectedResult, actualResult);
-    })
+        it(`provided value isn't string value`, () => {
+            suite.testData = {
+                some: "object"
+            };
+    
+            const actualResult = parse(suite.testData);
+            
+            assert.isUndefined(actualResult);
+        });
+
+        it('no data are provided', () => {
+            suite.testData = '';
+            const expectedResult = [];
+    
+            const actualResult = parse(suite.testData);
+            
+            assert.deepEqual(expectedResult, actualResult);
+        });
+
+        it('no row of data is provided', () => {
+            suite.testData = `"h1","h2","h3"`;
+            const expectedResult = [];
+    
+            const actualResult = parse(suite.testData);
+            
+            assert.deepEqual(expectedResult, actualResult);
+        });
+
+        it('no row of data is provided with new line character', () => {
+            suite.testData = `"h1","h2","h3"\n`;
+            const expectedResult = [];
+    
+            const actualResult = parse(suite.testData);
+            
+            assert.deepEqual(expectedResult, actualResult);
+        });
+
+        it('one row of data is provided', () => {
+            suite.testData = `"h1","h2","h3"\n"data1","data2","data3`;
+            const expectedResult = [
+                {"h1":"data1","h2":"data2","h3":"data3"}
+            ];
+    
+            const actualResult = parse(suite.testData);
+            
+            assert.deepEqual(expectedResult, actualResult);
+        });
+
+        it('multiples rows of datas are provided', () => {
+            suite.testData = `"h1","h2","h3"\n"data1","data2","data3"\n"data4","data5","data6"`;
+            const expectedResult = [
+                {"h1":"data1","h2":"data2","h3":"data3"},
+                {"h1":"data4","h2":"data5","h3":"data6"}
+            ];
+    
+            const actualResult = parse(suite.testData);
+            
+            assert.deepEqual(expectedResult, actualResult);
+        });
+
+    });
+    
 })
