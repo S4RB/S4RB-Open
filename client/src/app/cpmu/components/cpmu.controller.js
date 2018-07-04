9ce94af4dc31971fd47cfa04857a9e47ad82f898
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2018-07-04 11:50:47 +0200

    Move mock data to CpmuService

--- client/src/app/cpmu/components/cpmu.controller.js
+++ client/src/app/cpmu/components/cpmu.controller.js
@@ -1,3 +1,9 @@
 export default class CpmuController {
+  static get $inject() {
+    return ['cpmuService'];
+  }
 
+  constructor(cpmuService) {
+    this.cpmuData = cpmuService.getCpmuData();
+  }
 }
--- client/src/app/cpmu/services/cpmuService.js
+++ client/src/app/cpmu/services/cpmuService.js
@@ -1,3 +1,15 @@
 export default class CpmuService {
+  constructor() {
+    this.cpmuData = [{
+      month: 'January 2018',
+      cpmu: 51.544,
+    }, {
+      month: 'February 2018',
+      cpmu: 13.65844,
+    }];
+  }
 
+  getCpmuData() {
+    return this.cpmuData;
+  }
 }
